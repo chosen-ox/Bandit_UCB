64b0c7c9a425b040091121aceb1f60877efc446a
chosen-ox <120090316@link.cuhk.edu.cn>
2022-12-30 16:53:41 +0800

    add test files

---
+++ plt_receiver_info.py
@@ -0,0 +1,25 @@
+import sys
+import matplotlib.pyplot as plt
+import numpy as np
+filename = sys.argv[1]
+f = open(filename, 'r')
+for i in range(6):
+    f.readline()
+rate_list = []
+line = f.readline().split()
+while len(line) != 0:
+    try:
+        rate = float(line[6])
+        if line[7] == "Kbits/sec":
+            print(rate)
+            rate >>= 3
+    except:
+        line = f.readline().split()
+        continue
+    rate_list.append(rate)
+    line = f.readline().split()
+print(rate_list)
+y = np.array(rate_list)
+# x = np.arange(554)
+plt.plot( y, 'ro')
+plt.show()
